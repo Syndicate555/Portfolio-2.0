972aeb2ec2773a19a7cea0c87bba18bc8225fe1f
Saffat Aziz <50319868+Syndicate555@users.noreply.github.com>
2020-02-01 02:13:51 -0500

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,18 @@
+<?php
+
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $email_from = 'saffataziz55@gmail.com';
+    $email_subject = "New Form Submission";
+    $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                        "User Message: $message.\n";
+    $to = "saffataziz55@gmail.com";
+
+    $headers = "Form: $email_form \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
+    mail($to, email_subject, $email_body, $headers);
+    header("Location: index.html");
+?>

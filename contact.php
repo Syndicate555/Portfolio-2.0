d71a904f74f4f0c967376170d4788d255cc40334
Saffat Aziz <50319868+Syndicate555@users.noreply.github.com>
2020-02-01 02:19:02 -0500

    Backend complete

--- contact.php
+++ contact.php
@@ -13,6 +13,6 @@
 
     $headers = "Form: $email_form \r\n";
     $headers .= "Reply-To: $visitor_email \r\n";
-    mail($to, email_subject, $email_body, $headers);
+    mail($to, $email_subject, $email_body, $headers);
     header("Location: index.html");
 ?>
